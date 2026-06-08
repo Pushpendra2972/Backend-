4d4ca1e21eda424a801b0fa0257cf6f41df4ba21
Pushpendra Kumar <pkverma2972@gmail.com>
2026-06-08 15:17:56 +0530

    utilities done

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js"
 
-console.log("MONGO URL =", process.env.MONGODB_URL);
-
  const connectDB = async () => {
     try{
          const connectionInstances = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise
+        .resolve(requestHandler(req, res, next))
+        .catch((error) => {error})
+    }
+}
+
+export default asyncHandler
+
+
+// const asyncHandler = async(fn) => (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+
+//     } catch (error) {
+//        res
+//        .status(error.code || 500)
+//        .json({
+//         sucess: false,
+//         message: error.message
+//        })
+       
+//     }
+// }
